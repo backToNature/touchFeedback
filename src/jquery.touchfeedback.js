660668026d50a8a5b84bb1045fb7266709371d26
hengwusanren <youchuangqiba@163.com>
2016-10-11 01:07:00 +0800

    upd8

--- src/jquery.touchfeedback.js
+++ src/jquery.touchfeedback.js
@@ -5,7 +5,7 @@
             bindProp        = _this._bindProp, 
             feedbackClass   = _this._feedbackClass;
 
-        _this.startFunc = function (e) {
+        var startFunc = function (e) {
             var event = e.changedTouches ? e.changedTouches[0] : e,
                 identifier = eventData[event.identifier] = {};
             identifier.startY = event.pageY;
@@ -14,17 +14,13 @@
             if (identifier.target) {
                 $(identifier.target).addClass(feedbackClass);
             }
-        };
-
-        _this.moveFunc = function (e) {
+        },  moveFunc = function (e) {
             var event = e.changedTouches ? e.changedTouches[0] : e,
                 identifier = eventData[event.identifier];
             if (event.target && Math.abs(identifier.startY - event.pageY) > vague) {
                 $(identifier.target).removeClass(feedbackClass);
             }
-        };
-
-        _this.cancelFunc = function (e) {
+        },  cancelFunc = function (e) {
             var event = e.changedTouches ? e.changedTouches[0] : e,
                 identifier = eventData[event.identifier];
             if (identifier.target) {
@@ -33,8 +29,14 @@
             delete eventData[event.identifier];
         };
 
-        _this.$container.on('touchstart', _this.startFunc).on('touchmove', _this.moveFunc)
-            .on('touchcancel', _this.cancelFunc).on('touchend', _this.cancelFunc);
+        _this._events = {
+            touchstart:     startFunc,
+            touchmove:      moveFunc,
+            touchcancel:    cancelFunc,
+            touchend:       cancelFunc
+        };
+
+        _this.$container.on(_this._events);
     };
 
     var TouchFeedback = function (el, option) {
@@ -47,8 +49,7 @@
 
     TouchFeedback.prototype.destory = function () {
         var _this = this;
-        _this.$container.off('touchstart', _this.startFunc).off('touchmove', _this.moveFunc)
-            .off('touchcancel', _this.cancelFunc).off('touchend', _this.cancelFunc);
+        _this.$container.off(_this._events);
     };
 
     $.fn.touchfeedback = function (el, option) {
